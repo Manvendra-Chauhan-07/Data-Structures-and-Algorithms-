5be5029c8ea0da3df13d057f4de9c3fb8c88e4d8
Manvendra-Chauhan-07 <chauhan.manvendra2024@gmail.com>
2026-09-10 18:26:20 +0530

    Time: 7 ms (15.83%), Space: 185.3 MB (39.95%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -12,27 +12,53 @@ class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
         
-        vector<int> arr;
+        int k1=k-1;
+        int k2=k-1;
 
-        ListNode *temp=head;
+        ListNode *traverse=head;
 
-        while(temp)
-        {
-            arr.push_back(temp->val);
-            temp=temp->next;
+        while(k1 && traverse){
+            traverse=traverse->next;
+            k1--;
         }
-        
 
-        swap(arr[k-1],arr[arr.size()-k]);
+        int value1=traverse->val;
 
-        temp=head;
+        ListNode *future=head;
+        ListNode *current=nullptr;
+        ListNode *previous=nullptr;
 
-        for(int i=0;i<arr.size();i++)
+        while(future)
         {
-            temp->val=arr[i];
-            temp=temp->next;
+            current=future;
+            future=future->next;
+            current->next=previous;
+            previous=current;
         }
+
+        while(k2 && current)
+        {
+            current=current->next;
+            k2--;
+        }
+
+        int value2=current->val;
+
+        ListNode *future1=previous;
+        ListNode *current1=nullptr;
+        ListNode *previous1=nullptr;
+
+        while(future1)
+        {
+            current1=future1;
+            future1=future1->next;
+            current1->next=previous1;
+            previous1=current1;
+        }
+
+        swap(current->val , traverse->val);
 
         return head;
+
     }
 };
